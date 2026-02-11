714b00db63a2589d4c90cab3f0f0ae69a73f1899
Kirill Puljavin <105564583+KirillPuljavin@users.noreply.github.com>
2026-02-11 02:18:17 +0100

    Impplement Server Invites and fix networking & auth issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ export default async function Header() {
               </a>
             </>
           ) : (
-            <a href="/.auth/login/github" className="btn btn-sm">
+            <a href="/.auth/login/google" className="btn btn-sm">
               Login
             </a>
           )}
